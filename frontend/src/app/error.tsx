c16e47be84cf1c2542be70a6f899e2bc4eaabefb
AhemdAboAbadi <ahmed.m.aboabadi@gmail.com>
2025-05-30 20:45:53 +0300

    Add Error and NotFound pages with corresponding components and global styles

---
+++ frontend/src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client"
+// Components
+import ErrorPage from "@/components/error/ErrorPage"
+
+const Error = () => {
+  return <ErrorPage />
+}
+
+export default Error
---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import NotFoundPage from "@/components/error/NotFoundPage"
+
+const NotFound = async () => {
+  return <NotFoundPage />
+}
+
+export default NotFound
