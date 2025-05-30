5c2fc37b33b40603855440e79db83a7dd3e444cd
AhemdAboAbadi <ahmed.m.aboabadi@gmail.com>
2025-05-30 21:01:29 +0300

    Refactor Home and ErrorPage components for improved layout and styling consistency

--- frontend/src/components/error/ErrorPage.tsx
+++ frontend/src/components/error/ErrorPage.tsx
@@ -8,15 +8,7 @@ const ErrorPage = () => {
             Error
           </h4>
           <p className="lText pt-4 text-center text-bgColorOne">Error</p>
-          <div className="padding-t-40">
-            {/* <Link
-              href="/"
-              className="py group theme-transition-3 flex w-fit items-center justify-center gap-2 rounded-32px bg-blue-500 px-4 py-3 text-base font-semibold capitalize text-white shadow-five hover:bg-black hover:shadow-five-theme lg:px-6 lg:py-3"
-            >
-              <span className="block shrink-0">Home</span>{" "}
-              <ArrowUpRight className="theme-transition-3 shrink-0 group-hover:rotate-45" />
-            </Link> */}
-          </div>
+          <div className="padding-t-40"></div>
         </div>
       </div>
     </div>
